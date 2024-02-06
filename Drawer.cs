2bf6b06f152b297d5c71e80915c84653df8442e2
yoz75 <levtannn@gmail.com>
2024-02-06 14:36:11 +0300

    Добавьте файлы проекта.

---
+++ Drawer.cs
@@ -0,0 +1,63 @@
+﻿
+using System.Drawing;
+using System.IO;
+
+namespace BinaryCodeToImageConverter
+{
+    public class Drawer
+    {
+        Bitmap Image;
+        public Drawer(Bitmap image)
+        {
+            Image = image;
+        }
+
+        public void DrawToBitmap(string sourcePath)
+        {
+            byte[] fileSource = File.ReadAllBytes(sourcePath);
+            Color[] Colors = new Color[fileSource.Length];
+            int lastFileSourceElement = fileSource.Length - 1;
+            byte r, g, b, a;
+
+            for(int i = 0; i < Colors.Length; i++)
+            {
+                r = g = b = a = 0;
+
+                if(i + 3 < lastFileSourceElement)
+                {
+                    r = fileSource[i + 3];
+                }
+
+                if(i + 2 < lastFileSourceElement)
+                {
+                    g = fileSource[i + 2];
+                }
+
+                if(i + 1 < lastFileSourceElement)
+                {
+                    b = fileSource[i + 1];
+                }
+
+                if(i < lastFileSourceElement)
+                {
+                    a = fileSource[i];
+                }
+                Colors[i] = Color.FromArgb(((a << 24) | (r << 16) | (g << 8) | b));
+            }
+
+            for(int y = 0; y < Image.Height; y++)
+            {
+                for(int x = 0; x < Image.Width; x++)
+                {
+                    if(x * Image.Width + y < Colors.Length)
+                    {
+                        Image.SetPixel(x, y, Colors[x * Image.Width + y]);
+                        continue;
+                    }
+                    Image.SetPixel(x, y, Color.White);
+                }
+            }
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿
+using Microsoft.Win32;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows;
+
+namespace BinaryCodeToImageConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string PathToFile;
+        private int Resolution;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void SourceFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            var result = dialog.ShowDialog();
+            if(result.Value == true)
+            {
+                PathToFile = dialog.FileName;
+            }
+        }
+
+        private void GenerateFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            PixelFormat format = PixelFormat.Format32bppRgb;
+            if(IsTransparentCheckBox.IsChecked.Value)
+            {
+                format = PixelFormat.Format32bppArgb;
+            }
+            Bitmap image = new Bitmap(Resolution, Resolution, format);
+            Drawer drawer = new Drawer(image);
+            
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "png file |*.png";
+            var result = dialog.ShowDialog();
+            drawer.DrawToBitmap(PathToFile);
+            if(result.Value)
+            {
+                image.Save(dialog.FileName, ImageFormat.Png);
+            }
+        }
+    
+
+        private void ResolutionTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            try
+            {
+                Resolution = int.Parse(ResolutionTextBox.Text);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ow, there is an error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
